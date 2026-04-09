d18ada3ef51589d7fffa118043f323d5cf12dc19
Mohamed Soubhi <eng.mohamed.soubhi@gmail.com>
2026-04-09 13:40:39 +0200

    fix: update aesd-assignments to commit with sh shebang fix for finder.sh

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,7 +7,7 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-AESD_ASSIGNMENTS_VERSION = f0740704aefb80bcc21e2580303f06f5cffaab44
+AESD_ASSIGNMENTS_VERSION = f300f03829c5dc3eeb9fe1508b34e81b2fb25744
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
