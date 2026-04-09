4f08cf140efccfe8daa43fbce38f03c831f8ecdd
Mohamed Soubhi <eng.mohamed.soubhi@gmail.com>
2026-04-09 12:45:58 +0200

    fix: correct external.mk variable name and remove quotes from aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,12 +7,12 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-AESD_ASSIGNMENTS_VERSION = 'f0740704aefb80bcc21e2580303f06f5cffaab44'
+AESD_ASSIGNMENTS_VERSION = f0740704aefb80bcc21e2580303f06f5cffaab44
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 #AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
-AESD_ASSIGNMENTS_SITE = 'git@github.com:mohamed-soubhi/assignment3p2-msoubhi.git'
+AESD_ASSIGNMENTS_SITE = git@github.com:mohamed-soubhi/assignment3p2-msoubhi.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
