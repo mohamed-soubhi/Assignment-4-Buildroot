12388e0e8149c4475b8c1879cdef3de10b7a4911
Mohamed Soubhi <eng.mohamed.soubhi@gmail.com>
2026-04-09 14:12:13 +0200

    fix: update aesd-assignments version with absolute path fixes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,7 +7,7 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-AESD_ASSIGNMENTS_VERSION = f300f03829c5dc3eeb9fe1508b34e81b2fb25744
+AESD_ASSIGNMENTS_VERSION = 286db0096be62102d3f9ebd05af59b9bb16f5895
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
